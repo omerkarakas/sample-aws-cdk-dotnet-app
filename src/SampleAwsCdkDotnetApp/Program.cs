85e17ea480c6bfe31092a3637e822802b4dc9920
omerkarakas <karakasomer@gmail.com>
2022-12-02 17:12:36 +0300

    Initial commit

---
+++ src/SampleAwsCdkDotnetApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.CDK;
+
+namespace SampleAwsCdkDotnetApp
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new SampleAwsCdkDotnetAppStack(app, "SampleAwsCdkDotnetAppStack");
+
+            app.Synth();
+        }
+    }
+}
