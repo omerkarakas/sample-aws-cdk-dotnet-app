56c4bb7c9a6788716e33a81f96667e844cedc571
omerkarakas <karakasomer@gmail.com>
2022-12-06 08:20:56 +0300

    sample aws cdk .net app added

---
+++ src/SampleAwsCdkDotnetApp/HitCounter.cs
@@ -0,0 +1,9 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.Lambda;
+using Constructs;
+
+namespace SampleAwsCdkDotnetApp
+{
+
+
+}
--- src/SampleAwsCdkDotnetApp/SampleAwsCdkDotnetAppStack.cs
+++ src/SampleAwsCdkDotnetApp/SampleAwsCdkDotnetAppStack.cs
@@ -1,24 +1,92 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.DynamoDB;
+using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.APIGateway;
 using Amazon.CDK.AWS.SNS;
 using Amazon.CDK.AWS.SNS.Subscriptions;
 using Amazon.CDK.AWS.SQS;
 using Constructs;
+using System.Collections.Generic;
+using Cdklabs.DynamoTableViewer;
 
 namespace SampleAwsCdkDotnetApp
 {
+
+public class HitCounterProps
+    {
+        // The function for which we want to count url hits
+        public IFunction Downstream { get; set; }
+    }
+
+    public class HitCounter : Construct
+    {
+        public Function Handler { get; }
+        public readonly Table MyTable;
+
+
+        public HitCounter(Construct scope, string id, HitCounterProps props) : base(scope, id)
+        {
+            var table = new Table(this, "Hits", new TableProps
+            {
+                PartitionKey = new Attribute
+                {
+                    Name = "path",
+                    Type = AttributeType.STRING
+                },
+                RemovalPolicy = RemovalPolicy.DESTROY
+
+            });
+            MyTable = table;
+
+            Handler = new Function(this, "HitCounterHandler", new FunctionProps
+            {
+                Runtime = Runtime.NODEJS_16_X,
+                Handler = "hitcounter.handler",
+                Code = Code.FromAsset("lambda"),
+                Environment = new Dictionary<string, string>
+                {
+                    ["DOWNSTREAM_FUNCTION_NAME"] = props.Downstream.FunctionName,
+                    ["HITS_TABLE_NAME"] = table.TableName
+                }
+            });
+
+           // Grant the lambda role read/write permissions to our table
+            table.GrantReadWriteData(Handler);   
+
+          // Grant the lambda role invoke permissions to the downstream function
+            props.Downstream.GrantInvoke(Handler);                     
+        }
+    }
+
     public class SampleAwsCdkDotnetAppStack : Stack
     {
         internal SampleAwsCdkDotnetAppStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-             // The CDK includes built-in constructs for most resource types, such as Queues and Topics.
-            var queue = new Queue(this, "SampleAwsCdkDotnetAppQueue", new QueueProps
+            var hello = new Function(this, "HelloHandler", new FunctionProps
             {
-                VisibilityTimeout = Duration.Seconds(300)
+                Runtime = Runtime.NODEJS_16_X,
+                Code = Code.FromAsset("lambda"),
+                Handler = "hello.handler"
             });
 
-            var topic = new Topic(this, "SampleAwsCdkDotnetAppTopic");
+            var helloWithCounter = new HitCounter(this, "HelloHitCounter", new HitCounterProps
+            {
+                Downstream = hello
+            });
 
-            topic.AddSubscription(new SqsSubscription(queue));
+            // defines an API Gateway REST API resource backed by our "hello" function.
+            new LambdaRestApi(this, "Endpoint", new LambdaRestApiProps
+            {
+                Handler = helloWithCounter.Handler
+            });      
+
+            // Defines a new TableViewer resource
+            new TableViewer(this, "ViewerHitCount", new TableViewerProps
+            {
+                Title = "Hello Hits",
+                Table = helloWithCounter.MyTable,
+                SortBy = "-hits"
+            });                  
         }
     }
 }
